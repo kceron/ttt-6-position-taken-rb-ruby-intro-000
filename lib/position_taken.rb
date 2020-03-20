7c7fea545dd8201bdc6ed65039ea7c2ebdd6f0fe
K Ceron <karemiceron@gmail.com>
2020-03-20 17:28:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 # code your #position_taken? method here!
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-index = 0
+
 def position_taken?(board, index)
    if index[0] == " " || ""
      return false 
